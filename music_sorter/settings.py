53b2f7cffd772c9946314bd0be4ea16547ee9bc5
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-10-09 21:01:22 +0100

    chore: add build script, render.yaml and update settings.py, requirements.txt

--- music_sorter/settings.py
+++ music_sorter/settings.py
@@ -14,14 +14,17 @@ from pathlib import Path
 import environ
 import os
 
+
 env = environ.Env()
 # reading .env file
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -49,13 +52,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'music_sorter.urls'
@@ -83,6 +86,7 @@ WSGI_APPLICATION = 'music_sorter.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+ENV = env('ENV', default='development')
 
 if env('ENV') == 'production':
     DATABASES = {
