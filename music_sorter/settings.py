e4d01112f3be42fd8820ca2696eaa4a7303420a3
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-10-09 19:15:45 +0100

    fix: directory structure for django and render

--- music_sorter/settings.py
+++ music_sorter/settings.py
@@ -14,15 +14,14 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# reading .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
